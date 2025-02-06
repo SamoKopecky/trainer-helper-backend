b3cf14665763547276fda33e2be9f1423f53eff5
Samuel Kopecky <samuel.kopecky@heureka.group>
2025-02-06 17:18:39 +0100

    temp
    Signed-off-by: Samuel Kopecky <samuel.kopecky@heureka.group>

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,11 +1,10 @@
 use api::timeslot::Api;
-use crud::timeslot::CRUDTimeslot;
 use db::Db;
 use entity::timeslot;
 use sea_orm::entity::prelude::*;
 use sea_orm::sqlx::types::chrono::Utc;
 
-use chrono::prelude::*;
+use chrono::TimeDelta;
 use entity::prelude::*;
 
 mod api;
@@ -16,34 +15,25 @@ mod settings;
 #[tokio::main]
 async fn main() {
     let app = Api::build().await;
-    // test_db().await;
+    let models = generate_sample_week().await;
+    let db = Db::build().await.unwrap();
+    let res = Timeslot::insert_many(models).exec(&db.pool).await;
+    println!("{:?}", res);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn test_db() {
-    let db = Db::build().await.unwrap();
+async fn generate_sample_week() -> Vec<timeslot::ActiveModel> {
+    let mut models: Vec<timeslot::ActiveModel> = vec![];
+    let time = Utc::now().naive_local();
+    const TRAINER_ID: i32 = 1;
+    const DURATION: i32 = 60;
 
-    let naive_now = Utc::now().naive_local();
-    let model = Timeslot::build(1, naive_now, 103);
-    let result = Timeslot::insert(model).exec(&db.pool).await;
-    println!("{:?}", result);
-    println!("{:?}", timeslot::Entity::find_by_id(1).one(&db.pool).await);
-
-    let start_date_time = Utc
-        .with_ymd_and_hms(2025, 1, 27, 00, 00, 00)
-        .unwrap()
-        .naive_local();
-    let end_date_time = Utc
-        .with_ymd_and_hms(2025, 1, 27, 23, 00, 00)
-        .unwrap()
-        .naive_local();
-    let result = CRUDTimeslot::get_by_range_date(&db.pool, start_date_time, end_date_time)
-        .await
-        .unwrap();
-    println!("{}", result.len());
-    for r in result {
-        println!("{:?}\n\n", r);
+    for _ in 0..7 {
+        let time = time + TimeDelta::days(1);
+        models.push(Timeslot::build(TRAINER_ID, time, DURATION));
     }
+    println!("{:?}", models);
+    models
 }
